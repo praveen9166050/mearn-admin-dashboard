2f28ab818abdd1f081ab47f966714ac503be1ce3
Praveen Kumar <praveen9166050@gmail.com>
2024-08-11 23:32:30 +0530

    Added user info to the sidebar and the navbar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
-import { Box, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, useTheme } from "@mui/material";
+import { Box, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, useTheme } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom"
 import FlexBetween from "./FlexBetween";
-import { AdminPanelSettingsOutlined, CalendarMonthOutlined, ChevronLeft, ChevronRightOutlined, Groups2Outlined, HomeOutlined, PieChartOutlined, PointOfSaleOutlined, PublicOutlined, ReceiptLongOutlined, ShoppingCartOutlined, TodayOutlined, TrendingUpOutlined } from "@mui/icons-material";
+import { AdminPanelSettingsOutlined, CalendarMonthOutlined, ChevronLeft, ChevronRightOutlined, Groups2Outlined, HomeOutlined, PieChartOutlined, PointOfSaleOutlined, PublicOutlined, ReceiptLongOutlined, SettingsOutlined, ShoppingCartOutlined, TodayOutlined, TrendingUpOutlined } from "@mui/icons-material";
+import profileImage from "../assets/profileImage.jpg"
 
 const navItems = [
   {
@@ -63,7 +64,7 @@ const navItems = [
   },
 ];
 
-function Sidebar({isNonMobile, drawerWidth, isSidebarOpen, setIsSidebarOpen}) {
+function Sidebar({user, isNonMobile, drawerWidth, isSidebarOpen, setIsSidebarOpen}) {
   const {pathname} = useLocation();
   const [active, setActive] = useState("");
   const navigate = useNavigate();
@@ -72,6 +73,7 @@ function Sidebar({isNonMobile, drawerWidth, isSidebarOpen, setIsSidebarOpen}) {
   useEffect(() => {
     setActive(pathname.substring(1));
   }, [pathname]);
+
   return (
     <Box component="nav">
       {isSidebarOpen && (
@@ -146,6 +148,39 @@ function Sidebar({isNonMobile, drawerWidth, isSidebarOpen, setIsSidebarOpen}) {
                 );
               })}
             </List>
+          </Box>
+          <Box position="absolute" bottom="2rem">
+            <Divider />
+            <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="40px"
+                width="40px"
+                borderRadius="50%"
+                sx={{objectFit: "cover"}}
+              />
+              <Box textAlign="left">
+                <Typography 
+                  fontWeight="bold"
+                  fontSize="0.9rem"
+                  sx={{color: theme.palette.secondary[100]}}
+                >
+                  {user.name}
+                </Typography>
+                <Typography 
+                  fontSize="0.8rem"
+                  sx={{color: theme.palette.secondary[200]}}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <SettingsOutlined sx={{
+                color: theme.palette.secondary[300],
+                fontSize: "25px"
+              }}/>
+            </FlexBetween>
           </Box>
         </Drawer>
       )}
--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -2,11 +2,17 @@ import { Box, useMediaQuery } from "@mui/material"
 import Navbar from "components/Navbar"
 import Sidebar from "components/Sidebar";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom"
+import { useGetUserQuery } from "state/api";
 
 function Layout() {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+  const {data} = useGetUserQuery(userId);
+  console.log("data:", data);
+  
   return (
     <Box 
       display={isNonMobile ? "flex" : "block"} 
@@ -14,13 +20,18 @@ function Layout() {
       width="100%"
     >
       <Sidebar 
+        user={(data && data.user) || {}}
         isNonMobile={isNonMobile}
         drawerWidth="250px"
         isSidebarOpen={isSidebarOpen}
         setIsSidebarOpen={setIsSidebarOpen}
       />
-      <Box>
-        <Navbar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen}/>
+      <Box flexGrow={1}>
+        <Navbar 
+          user={(data && data.user) || {}}
+          isSidebarOpen={isSidebarOpen} 
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
         <Outlet />
       </Box>
     </Box>
