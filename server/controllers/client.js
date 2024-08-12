abf404ccea4ab7ee1a727eb651951abb3fa18120
Praveen Kumar <praveen9166050@gmail.com>
2024-08-12 23:48:10 +0530

    Added get customers functionality to API and UI

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,5 +1,6 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
 
 export const getProducts = async (req, res, next) => {
   try {
@@ -14,6 +15,18 @@ export const getProducts = async (req, res, next) => {
     res.status(200).json({
       success: true,
       productsWithStats
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const getCustomers = async (req, res, next) => {
+  try {
+    const customers = await User.find({role: "user"}).select("-password");
+    res.status(200).json({
+      success: true,
+      customers
     });
   } catch (error) {
     next(error);
