5947337ac914da2973337210b658532636dcf723
Praveen Kumar <praveen9166050@gmail.com>
2024-08-15 22:09:39 +0530

    Added geography API and geography page UI

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -2,6 +2,7 @@ import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import Transaction from "../models/Transaction.js";
 import User from "../models/User.js";
+import getCountryIso3 from "country-iso-2-to-3";
 
 export const getProducts = async (req, res, next) => {
   try {
@@ -60,6 +61,32 @@ export const getTransactions = async (req, res, next) => {
       success: true,
       transactions,
       total
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const getGeography = async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    const mappedLocations = users.reduce((acc, {country}) => {
+      const countryIso3 = getCountryIso3(country);
+      if (!acc[countryIso3]) {
+        acc[countryIso3] = 0;
+      }
+      acc[countryIso3]++;
+      return acc;
+    }, {});
+    const formattedLocations = Object.entries(mappedLocations).map(([country, count]) => {
+      return {
+        id: country,
+        value: count
+      };
+    });
+    res.status(200).json({
+      success: true,
+      locations: formattedLocations
     });
   } catch (error) {
     next(error);
