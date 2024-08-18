35a9e38e4a3c692213d4081f7fc54fcc24d8f8e5
Praveen Kumar <praveen9166050@gmail.com>
2024-08-18 12:16:44 +0530

    Added get performance API

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
 
 export const getAdmins = async (req, res, next) => {
   try {
@@ -6,6 +8,41 @@ export const getAdmins = async (req, res, next) => {
     res.status(200).json({
       success: true,
       admins
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const getUserPerformance = async (req, res, next) => {
+  try {
+    const {id} = req.params;
+    const userWithStats = await User.aggregate([
+      {
+        $match: {_id: new mongoose.Types.ObjectId(id)}
+      },
+      {
+        $lookup: {
+          from: 'affiliatestats',
+          localField: '_id',
+          foreignField: 'userId',
+          as: 'affiliateStats'
+        }
+      },
+      {
+        $unwind: '$affiliateStats'
+      }
+    ]);
+    const saleTransactions = await Promise.all(
+      userWithStats[0].affiliateStats.affiliateSales.map((id) => {
+        return Transaction.findById(id);
+      })
+    );
+    const filteredSaleTransactions = saleTransactions.filter(transaction => transaction !== null);
+    res.status(200).json({
+      success: true,
+      user: userWithStats,
+      sales: filteredSaleTransactions
     });
   } catch (error) {
     next(error);
--- server/index.js
+++ server/index.js
@@ -10,12 +10,13 @@ import managementRouter from "./routes/management.js";
 import salesRouter from "./routes/sales.js";
 
 // To insert bulk user data
-// import { dataOverallStat, dataProduct, dataProductStat, dataTransaction, dataUser } from "./data/index.js";
+// import { dataAffiliateStat, dataOverallStat, dataProduct, dataProductStat, dataTransaction, dataUser } from "./data/index.js";
 // import User from "./models/User.js";
 // import Product from "./models/Product.js";
 // import ProductStat from "./models/ProductStat.js";
 // import Transaction from "./models/Transaction.js";
 // import OverallStat from "./models/OverallStat.js";
+// import AffiliateStat from "./models/AffiliateStat.js";
 
 dotenv.config();
 
@@ -53,6 +54,7 @@ mongoose.connect(process.env.MONGO_URI)
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
     // OverallStat.insertMany(dataOverallStat);
+    // AffiliateStat.insertMany(dataAffiliateStat);
   });
 })
 .catch(error => console.log(error));
