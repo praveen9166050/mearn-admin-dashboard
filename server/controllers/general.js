52f5da7aeca6163906bb7557dbeb093252f6ea89
Praveen Kumar <praveen9166050@gmail.com>
2024-08-18 12:57:25 +0530

    Added dashboard stats API

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,3 +1,5 @@
+import OverallStat from "../models/OverallStat.js";
+import Transaction from "../models/Transaction.js";
 import User from "../models/User.js";
 
 export const getUser = async (req, res, next) => {
@@ -7,6 +9,40 @@ export const getUser = async (req, res, next) => {
     res.status(200).json({
       success: true,
       user
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const getDashboardStats = async (req, res, next) => {
+  try {
+    // hardcoded values
+    const currentMonth = "November";
+    const currentYear= 2021;
+    const currentDay = "2021-11-15";
+
+    // recent transactions
+    const transactions = await Transaction.find({}).limit(50).sort({createdOn: -1});
+
+    // overall stats
+    const overallStats = await OverallStat.find({year: currentYear});
+
+    const {totalCustomers, yearlyTotalSoldUnits, yearlySalesTotal, monthlyData, salesByCategory} = overallStats[0];
+    
+    const thisMonthStats = overallStats[0].monthlyData.find(({month}) => month === currentMonth);
+    const todayStats = overallStats[0].dailyData.find(({date}) => date === currentDay);
+    
+    res.status(200).json({
+      success: true,
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+      thisMonthStats,
+      todayStats,
+      transactions
     });
   } catch (error) {
     next(error);
